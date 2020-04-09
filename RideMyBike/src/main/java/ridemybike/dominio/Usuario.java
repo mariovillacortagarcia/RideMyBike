00e99827f37186ccfda23d079a058b530cf46351
davidmd <davidmelendezdiez99@gmail.com>
2020-04-09 18:31:14 +0200

    Paquetes

---
+++ RideMyBike/src/main/java/ridemybike/dominio/Usuario.java
@@ -0,0 +1,18 @@
+package ridemybike.dominio;
+
+/**
+ * Implementacion de un usuario
+ * 
+ */
+public class Usuario {
+    private String nombre;
+    private String apellidos;
+    private String dni;
+    private String email;
+    private long tlf;
+    private String nombreUsuario;
+    private String tarjetaCredito;
+    private String hashContrasena;
+    
+   
+}
