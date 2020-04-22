bb48ecb6205524d81a38c83594c23ed6bca9c9bb
Mario Villacorta <Mario Villacorta@192.168.1.188>
2020-04-22 17:00:15 +0200

    Terminado servlet de alquileres

--- RideMyBike/src/main/java/ridemybike/servlets/Viajes.java
+++ RideMyBike/src/main/java/ridemybike/servlets/Viajes.java
@@ -7,10 +7,13 @@ package ridemybike.servlets;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import ridemybike.dominio.Alquiler;
+import ridemybike.dominio.db.AlquilerDB;
 
 /**
  *
@@ -30,18 +33,15 @@ public class Viajes extends HttpServlet {
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet Viajes</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet Viajes at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
+        
+        Alquiler alquileres[] = (Alquiler [])AlquilerDB.selectAllAlquiler().toArray();
+        request.setAttribute("alquileres", alquileres);
+        
+        String url = "/viajes.jsp";
+        RequestDispatcher dispatcher = getServletContext().getRequestDispatcher(url);
+        dispatcher.forward(request, response);
+        
+        
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
