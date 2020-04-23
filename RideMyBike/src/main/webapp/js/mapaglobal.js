e3749d29e4c871532e43d0e247bae6372a97d77b
Mario Villacorta <Mario Villacorta@192.168.1.188>
2020-04-23 18:50:23 +0200

    Configurado mapa

--- RideMyBike/src/main/webapp/js/mapaglobal.js
+++ RideMyBike/src/main/webapp/js/mapaglobal.js
@@ -3,6 +3,8 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
+//Creacion del mapa
 const PROVEEDOR = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
 const VALLADOLID = [41.6520, -4.7286];
 const ZOOM = 13;
@@ -11,3 +13,5 @@ var mapa =  L.map('mapaglobal').setView(VALLADOLID, ZOOM);
 L.tileLayer(PROVEEDOR, {
     attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(mapa);
+
+//Obtencion de las coordenadas de las bicis
