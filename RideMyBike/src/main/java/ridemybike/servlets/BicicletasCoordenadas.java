ab1abd724968ddda2d6445c5b6ae334aa0904c34
Mario Villacorta <Mario Villacorta@192.168.1.188>
2020-04-24 17:31:41 +0200

    Servlet para obtener JSON con coordenadas de bicicletas

---
+++ RideMyBike/src/main/java/ridemybike/servlets/BicicletasCoordenadas.java
@@ -0,0 +1,101 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ridemybike.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import ridemybike.dominio.Bicicleta;
+import ridemybike.dominio.db.BicicletaDB;
+
+
+/**
+ *
+ * @author Mario Villacorta
+ */
+@WebServlet(name = "BicicletasCoordenadas", urlPatterns = {"/BicicletasCoordenadas"})
+public class BicicletasCoordenadas extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        ArrayList<Bicicleta> bicicletas = (ArrayList<Bicicleta>)BicicletaDB.selectAllBicicleta();
+        String ubicaciones ="[";
+        String ubicacion = null;
+        String lat;
+        String lon;
+        for(Bicicleta bicicleta : bicicletas){
+           lat = Double.toString(bicicleta.getLatitud());
+           lon = Double.toString(bicicleta.getLongitud());
+           ubicacion = "{'lat':"+lat+", 'lon':"+ lon+"},";
+           ubicaciones += ubicacion;
+        }
+        if(ubicacion != null)
+            ubicaciones = ubicaciones.substring(0, ubicaciones.length()-1);
+        ubicaciones += "]";
+
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.print(ubicaciones);
+            out.flush();
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
