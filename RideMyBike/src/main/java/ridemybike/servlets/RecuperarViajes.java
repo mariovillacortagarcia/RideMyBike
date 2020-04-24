7cd655dcc2776e358e0a89d434d81eb2ed4efffd
Mario Villacorta <Mario Villacorta@192.168.1.188>
2020-04-24 16:23:35 +0200

    Renombrado servlet RecuperarViajes

---
+++ RideMyBike/src/main/java/ridemybike/servlets/RecuperarViajes.java
@@ -0,0 +1,95 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ridemybike.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import ridemybike.dominio.Alquiler;
+import ridemybike.dominio.db.AlquilerDB;
+
+/**
+ *
+ * @author mario
+ */
+@WebServlet(name = "RecuperarViajes", urlPatterns = {"/RecuperarViajes"})
+public class RecuperarViajes extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        
+        Alquiler alquileres[] = (Alquiler [])AlquilerDB.selectAllAlquiler().toArray();        
+        request.setAttribute("alquileres", alquileres);
+        
+        String url = "/viajes.jsp";
+        RequestDispatcher dispatcher = getServletContext().getRequestDispatcher(url);
+        dispatcher.forward(request, response);
+        
+        url = "/viajes_en_proceso.jsp";
+        dispatcher = getServletContext().getRequestDispatcher(url);
+        dispatcher.forward(request, response);
+        
+        url = "/viajes_archivados.jsp";
+        dispatcher = getServletContext().getRequestDispatcher(url);
+        dispatcher.forward(request, response);
+        
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
