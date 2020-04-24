b36df745619eb9110d2839c25b168136d1f4c3f3
Mario Villacorta <Mario Villacorta@192.168.1.188>
2020-04-24 17:50:33 +0200

    Terminado script mapa

--- RideMyBike/src/main/webapp/js/mapaglobal.js
+++ RideMyBike/src/main/webapp/js/mapaglobal.js
@@ -9,9 +9,20 @@ const PROVEEDOR = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
 const VALLADOLID = [41.6520, -4.7286];
 const ZOOM = 13;
 
-var mapa =  L.map('mapaglobal').setView(VALLADOLID, ZOOM);
+var mapa = L.map('mapaglobal').setView(VALLADOLID, ZOOM);
 L.tileLayer(PROVEEDOR, {
     attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(mapa);
 
-//Obtencion de las coordenadas de las bicis
+//Obtencion de las coordenadas de las bicis a traves de peticion al servler
+$.get('${pageContext.request.contextPath}/BicicletasCoordenadas', function (data) {
+    var ubicaciones = JSON.parse(data);
+    for (i = 0; i < ubicaciones.length; i++) {
+        var lat = ubicaciones[i].lat;
+        var lon = ubicaciones[i].lon;
+        var marcador = L.marker([lat, lon]).addTo(mapa).on('click', function (e) {
+            $("#bicicletaSeleccionada").text(e.latlng);
+        });
+        ;
+    }
+});
