c2ede98387ef2287d65d7972b758e61c582ae9ac
Mario Villacorta <Mario Villacorta@192.168.1.188>
2020-05-20 17:18:43 +0200

    Muestra imagenes de bicics

---
+++ RideMyBike/src/main/java/ridemybike/servlets/RecuperarImagenBicicleta.java
@@ -0,0 +1,40 @@
+package ridemybike.servlets;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import ridemybike.dominio.db.BicicletaDB;
+import ridemybike.dominio.db.UsuarioDB;
+
+/**
+ * Implementacion del servlet para recuperar la foto de perfil de un usuario
+ * a partir de su nombre de usuario
+ */
+@WebServlet(name = "RecuperarImagenBicicleta", urlPatterns = {"/RecuperarImagenBicicleta"})
+public class RecuperarImagenBicicleta extends HttpServlet {
+    public void processRequest(HttpServletRequest request,HttpServletResponse response)throws ServletException, IOException {
+        response.setContentType("image/jpg");
+        OutputStream respuesta = response.getOutputStream();
+        String idBici = request.getParameter("idBici");
+        BicicletaDB.getImagen(Integer.parseInt(idBici), respuesta);
+        respuesta.close();
+        response.flushBuffer();
+    }
+    
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+    
+}
