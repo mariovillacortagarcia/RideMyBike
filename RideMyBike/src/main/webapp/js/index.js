6648be28015c78963db5c5637a081246da7f6403
Jorge <j.ch.f.jorge11@hotmail.com>
2020-05-22 19:47:04 +0200

    Mod "index.js" actualiza al pulsar "btNext" y precio actualiza dinámico.

--- RideMyBike/src/main/webapp/js/index.js
+++ RideMyBike/src/main/webapp/js/index.js
@@ -1,21 +1,15 @@
-$('#fechaInicioPrestamo').change(function() {
-    document.getElementById('textoFechaInicio').innerHTML = document.getElementById('fechaInicioPrestamo').value;
-});
-
-$('#horaInicioPrestamo').change(function() {
-    document.getElementById('textoHoraInicio').innerHTML = document.getElementById('horaInicioPrestamo').value;
-});
-
-$('#fechaFinPrestamo').change(function() {
-    document.getElementById('textoFechaFin').innerHTML = document.getElementById('fechaFinPrestamo').value;
-});
-
-$('#horaFinPrestamo').change(function() {
-    document.getElementById('textoHoraFin').innerHTML = document.getElementById('horaFinPrestamo').value;
-    document.getElementById('textoPrecioTotal').innerHTML = 2.3;
-});
 
 $('#bicicletaSeleccionada').change(function() {
     document.getElementById('textoBiciSeleccionada').innerHTML = document.getElementById('bicicletaSeleccionada').value;
 });
 
+$('#BtNext').click(function() {
+    document.getElementById('textoFechaInicio').innerHTML = document.getElementById('fechaInicioPrestamo').value;
+    document.getElementById('textoHoraInicio').innerHTML = document.getElementById('horaInicioPrestamo').value;
+    document.getElementById('textoFechaFin').innerHTML = document.getElementById('fechaFinPrestamo').value;
+    document.getElementById('textoHoraFin').innerHTML = document.getElementById('horaFinPrestamo').value;
+    var tiempoInicial = Date.parse(document.getElementById('fechaInicioPrestamo').value +"T"+ document.getElementById('horaInicioPrestamo').value);
+    var tiempoFinal = Date.parse(document.getElementById('fechaFinPrestamo').value +"T"+ document.getElementById('horaFinPrestamo').value);
+    var tiempo = (tiempoFinal - tiempoInicial)/1000/60;
+    document.getElementById('textoPrecioTotal').innerHTML = (tiempo * 0.01);
+});
